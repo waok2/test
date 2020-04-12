7bd7e7c8687806d9af66cb2146feedc29978d82f
waok2 <abachwayne@live.com>
2020-04-12 22:36:55 +0300

    4 tests corrected flak8

--- app/core/models.py
+++ app/core/models.py
@@ -1,29 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-   
-   def create_user(self, email, password=None, **extra_fields):
-    """ Creates and saves new user"""
-    if not email:
-        raise ValueError('users musht have an email address')
-    user = self.model(email=self.normalize_email(email), **extra_fields)
-    user = self.model(email=self.normalize_email(email), **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
 
-    return user
-
-   def create_superuser(self, email, password):
-        """Creates and saves a new super user"""
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
+    def create_user(self, email, password=None, **extra_fields):
+        """ Creates and saves new user"""
+        if not email:
+            raise ValueError('users musht have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+            """Creates and saves a new super user"""
+            user = self.create_user(email, password)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save(using=self._db)
+
+            return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -36,4 +36,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    
+
     def test_create_user_with_email_successful(self):
         """ Testing creating a new user with an email is succeful"""
         email = 'test@wwyne.com'
@@ -16,7 +16,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@WWWWW.com'
@@ -24,20 +23,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123') 
-            
+            get_user_model().objects.create_user(None, 'test123')
+
     def test_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-        'test@londonappdev.com',
-        'test123'
+          'test@londonappdev.com',
+          'test123'
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
-        
